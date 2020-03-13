f4d0f64fd4ed043132d87948b741c3fd193f8201
TaeKwan Jung <50404450+TKJung@users.noreply.github.com>
2020-03-14 03:28:12 +0900

    Create ptrarray.c

---
+++ C-Programming Review/28110.Pointer.Array3/ptrarray.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main()
+{
+    char *ps[3];
+    char buf[80];
+
+    for(int i = 0; i < 3; i++)
+    {
+        scanf("%s", buf);
+
+        ps[i] = (char*)malloc(strlen(buf) + 1);
+        strcpy(ps[i], buf);
+    }
+
+    for( int i = 2; i >= 0; i--)
+    {
+        printf("%s\n", ps[i]);
+    }
+
+}
